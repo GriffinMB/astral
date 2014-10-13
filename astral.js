b2812b75160f86e2eeff629de01bc4c73ee784b9
GriffinMB <byatt.griffin@gmail.com>
2014-10-13 10:44:18 -0400

    Update version

--- astral.js
+++ astral.js
@@ -7,7 +7,7 @@ var touch = require('touch');
 var exec = require('child_process').exec;
 
 program
-  .version('0.0.5')
+  .version('0.0.6')
   .usage('project <projectName>')
   .option('-r, --routes', 'enable iron-router')
   .option('-f, --full', 'include all packages')
